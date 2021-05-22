1d435e0b1157750c094205d220a31371df0e1f42
Arshad <arshadpinjari666@gmail.com>
2021-05-22 17:06:45 +0530

    Added.FindMaxByGeneric

--- FindMaximumNumber/Program.cs
+++ FindMaximumNumber/Program.cs
@@ -8,14 +8,24 @@ namespace FindMaximumNumber
         {
             Console.WriteLine("Finding Maximum Number");
 
-            int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
-            Console.WriteLine("Maximum Integer Number:" +output);
+            //int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
+            //Console.WriteLine("Maximum Integer Number:" +output);
 
-            double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(111.2, 22.45, 78.56);
-            Console.WriteLine("Maximum Double Number:"+doubleoutput);
+            //double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(111.2, 22.45, 78.56);
+            //Console.WriteLine("Maximum Double Number:"+doubleoutput);
 
-            string stringoutput = MaximumNumberCheck.MaximumStringNumber("Apple", "Peach", "Banana");
-            Console.WriteLine("Maximum Value String:" +stringoutput);
+            //string stringoutput = MaximumNumberCheck.MaximumStringNumber("Apple", "Peach", "Banana");
+            //Console.WriteLine("Maximum Value String:" +stringoutput);
+
+            int[] intArray = { 112, 344, 432, 555, 678 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+            generic.PrintMaxValue();
+            double[] doubleArray = { 11.2, 34.4, 4.32, 55.5, 6.78 };
+            GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+            genericDouble.PrintMaxValue();
+            string[] stringArray = { "Apple", "Peach", "Banana", "Watermelon", "Dragonfruit" };
+            GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
+            genericString.PrintMaxValue();
         }
     }
 }
